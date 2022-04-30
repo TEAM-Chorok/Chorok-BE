2174dd7e299e6bd05f04ff199a25e37f3eac2d07
kim-ju-ho <kim1jo2ho3@naver.com>
2022-04-30 10:53:38 +0900

    프로젝트 초기 세팅 및 폴더구조 생성

---
+++ src/main/java/com/finalproject/chorok/ChorokApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.chorok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChorokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChorokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finalproject/chorok/ChorokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.chorok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChorokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
