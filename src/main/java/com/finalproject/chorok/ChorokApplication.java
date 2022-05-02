d51d446fe3d42e3da5cad708331b961b9838fedc
lincoln-burrows <minseong.kim7000@gmail.com>
2022-05-02 20:29:45 +0900

    [Feat]common폴더 아래에 Image 폴더 추가

--- src/main/java/com/finalproject/chorok/ChorokApplication.java
+++ src/main/java/com/finalproject/chorok/ChorokApplication.java
@@ -2,12 +2,22 @@ package com.finalproject.chorok;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ChorokApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.properties,"
+            + "classpath:aws.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(ChorokApplication.class, args);
+        new SpringApplicationBuilder(ChorokApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
 }
+
