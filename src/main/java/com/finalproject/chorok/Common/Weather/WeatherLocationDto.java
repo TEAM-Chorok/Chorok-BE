ed96b24fe68dd49cbb0751704c962f6d0c35270d
lincoln-burrows <minseong.kim7000@gmail.com>
2022-05-09 08:10:18 +0900

    [Style]날씨api 파라미터 requestbody로 수정

---
+++ src/main/java/com/finalproject/chorok/Common/Weather/WeatherLocationDto.java
@@ -0,0 +1,11 @@
+package com.finalproject.chorok.Common.Weather;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WeatherLocationDto {
+    private String lat;
+    private String lon;
+}
