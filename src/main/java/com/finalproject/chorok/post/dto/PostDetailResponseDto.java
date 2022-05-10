5838fe6689ef358a29360cebc5626bb95f24bc18
kim-ju-ho <kim1jo2ho3@naver.com>
2022-05-11 02:05:54 +0900

    fix : merge bug 수정

--- src/main/java/com/finalproject/chorok/post/dto/PostDetailResponseDto.java
+++ src/main/java/com/finalproject/chorok/post/dto/PostDetailResponseDto.java
@@ -1,10 +1,10 @@
 package com.finalproject.chorok.post.dto;
 
 
-import com.finalproject.chorok.post.model.Comment;
-import com.finalproject.chorok.post.model.Post;
 import com.finalproject.chorok.common.utils.CaluateTime;
 import com.finalproject.chorok.plant.model.PlantPlace;
+import com.finalproject.chorok.post.dto.comment.CommentResponseDto;
+import com.finalproject.chorok.post.model.Post;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -32,30 +32,55 @@ public class PostDetailResponseDto {
     private String postTitle;
     private String postContent;
     private String postImgUrl;
-    private String postRecentTime;
+    // 게시판 타입
+    private String postType;
     // 식물위치
-    private String postPlace;
+    private String PlantPlace;
+    private String postRecentTime;
     // 게시글 좋아요
-    private Boolean PostLike;
+    private Boolean postLike;
     private int postLikeCount;
     // 북마크
     private Boolean postBookMark;
     // 댓글 - Comment
-    List<Comment> commentList;
+    List<CommentResponseDto> commentList;
 
 
-    public PostDetailResponseDto(Post post, PlantPlace plantPlace) {
+    // 플렌테리어
+    public PostDetailResponseDto(Post post, PlantPlace plantPlace,Boolean likePostChk, Boolean bookMarkPostChk, List<CommentResponseDto> commentResponseDtos) {
         this.postId=post.getPostId();
         this.nickname=post.getUser().getNickname();
         this.profileImgUrl=post.getUser().getProfileImageUrl();
         this.postTitle=post.getPostTitle();
         this.postContent=post.getPostContent();
         this.postImgUrl=post.getPostImgUrl();
-        this.postPlace = plantPlace.getPlantPlace();
+        this.postType = post.getPostType().getPostType();
+        this.PlantPlace = plantPlace.getPlantPlace();
         this.postRecentTime= CaluateTime.calculateTime(Timestamp.valueOf(post.getCreatedAt()));
-      //  this.commentList=post.getCommentList();
         // 내가 게시글에 좋아요를 눌렀는지 확인
+        this.postLike=likePostChk;
         // 내가 게시글에 북마크를 눌렀는지 확인
+        this.postBookMark=bookMarkPostChk;
         this.postLikeCount=post.getPostLike().size();
+        this.commentList=commentResponseDtos;
+
+    }
+
+    // 게시판
+    public PostDetailResponseDto(Post post, Boolean likePostChk, Boolean bookMarkPostChk, List<CommentResponseDto> commentResponseDtos) {
+        this.postId=post.getPostId();
+        this.nickname=post.getUser().getNickname();
+        this.profileImgUrl=post.getUser().getProfileImageUrl();
+        this.postTitle=post.getPostTitle();
+        this.postContent=post.getPostContent();
+        this.postImgUrl=post.getPostImgUrl();
+        this.postType = post.getPostType().getPostType();
+        this.postRecentTime= CaluateTime.calculateTime(Timestamp.valueOf(post.getCreatedAt()));
+        // 내가 게시글에 좋아요를 눌렀는지 확인
+        this.postLike=likePostChk;
+        // 내가 게시글에 북마크를 눌렀는지 확인
+        this.postBookMark=bookMarkPostChk;
+        this.postLikeCount=post.getPostLike().size();
+        this.commentList=commentResponseDtos;
     }
 }
--- src/main/java/com/finalproject/chorok/post/dto/PostResponseDto.java
+++ src/main/java/com/finalproject/chorok/post/dto/PostResponseDto.java
@@ -1,5 +1,7 @@
 package com.finalproject.chorok.post.dto;
 
+import com.finalproject.chorok.login.model.User;
+import com.finalproject.chorok.post.model.Post;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,10 +23,29 @@ public class PostResponseDto {
     private Long postId;
     private String postTitle;
     private String postImgUrl;
+    private String postContent;
+    private String nickname;
+    private Long userId;
+    private String profileImgUrl;
 
-    public PostResponseDto(Long postId, String postTitle, String postImgUrlNo) {
+
+
+    public PostResponseDto(Long postId, String postTitle, String postImgUrl, String postContent, User user) {
         this.postId = postId;
         this.postTitle = postTitle;
-        this.postImgUrl = postImgUrlNo;
+        this.postImgUrl = postImgUrl;
+        this.postContent=postContent;
+        this.nickname=user.getNickname();
+    }
+
+    public PostResponseDto(Post post) {
+        this.postId = post.getPostId();
+        this.postTitle = post.getPostTitle();
+        this.postImgUrl = post.getPostImgUrl();
+        this.postContent=post.getPostContent();
+        this.nickname=post.getUser().getNickname();
+        this.userId = post.getUser().getUserId();
+        this.profileImgUrl=post.getUser().getProfileImageUrl();
+
     }
 }
