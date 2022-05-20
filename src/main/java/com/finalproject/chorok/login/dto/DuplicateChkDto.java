59a4e41e71e0651f2a6c60a7cc266ce85c70b4bd
lincoln-burrows <minseong.kim7000@gmail.com>
2022-05-20 22:43:40 +0900

    [Fix]중복검사 DTO 수정(기본생성자)

--- src/main/java/com/finalproject/chorok/login/dto/DuplicateChkDto.java
+++ src/main/java/com/finalproject/chorok/login/dto/DuplicateChkDto.java
@@ -2,10 +2,12 @@ package com.finalproject.chorok.login.dto;
 
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class DuplicateChkDto {
     private String username;
     private String nickname;
