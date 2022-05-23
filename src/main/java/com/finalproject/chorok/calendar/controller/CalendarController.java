78f7d72a20dc8a9199cca65b1a140d37b6650cae
Eunah323 <unapretty@gmail.com>
2022-05-23 15:27:27 +0900

    [fit] 달력에서 투두취소하기 추가 + cascade추가

--- src/main/java/com/finalproject/chorok/calendar/controller/CalendarController.java
+++ src/main/java/com/finalproject/chorok/calendar/controller/CalendarController.java
@@ -2,9 +2,11 @@ package com.finalproject.chorok.calendar.controller;
 
 import com.finalproject.chorok.calendar.Dto.CalendarResponseDto;
 import com.finalproject.chorok.calendar.service.CalendarService;
+import com.finalproject.chorok.login.model.User;
 import com.finalproject.chorok.security.UserDetailsImpl;
 import com.finalproject.chorok.todo.dto.SprayingDayResponstDto;
 import com.finalproject.chorok.todo.model.Todo;
+import com.finalproject.chorok.todo.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,22 +20,22 @@ import java.time.LocalDate;
 public class CalendarController {
 
     private final CalendarService calendarService;
-
+    private final TodoRepository todoRepository;
 
 
     @GetMapping("/calendar/{yearmonth}/{myPlantNo}")
-    public CalendarResponseDto getMonthlyrecord(@PathVariable String yearmonth, @PathVariable Long myPlantNo, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ResponseEntity<CalendarResponseDto> getMonthlyrecord(@PathVariable String yearmonth, @PathVariable Long myPlantNo, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         String year = yearmonth.substring(0, 4);
         String month = yearmonth.substring(4);
         String start0 = year + "-" + month + "-01";
         LocalDate start = LocalDate.parse(start0);
         LocalDate end = start.withDayOfMonth(start.lengthOfMonth());
-        return calendarService.getMonthly(myPlantNo, start, end, userDetails);
+        return ResponseEntity.status(HttpStatus.OK).body(calendarService.getMonthly(myPlantNo, start, end, userDetails));
 
     }
 
     @PatchMapping("/calendar/{yearmonthday}/{myPlantNo}/{workType}")
-    public ResponseEntity<Todo> checkTodoOkInCalendar(@PathVariable String yearmonthday, @PathVariable String workType, @PathVariable Long myPlantNo, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ResponseEntity<?> checkTodoOkInCalendar(@PathVariable String yearmonthday, @PathVariable String workType, @PathVariable Long myPlantNo, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         String year = yearmonthday.substring(0, 4);
         String month = yearmonthday.substring(4, 6);
         String day = yearmonthday.substring(6);
@@ -43,26 +45,48 @@ public class CalendarController {
         return ResponseEntity.status(HttpStatus.OK).body(calendarService.checkTodoInCalendar(thatDay, workType, myPlantNo, userDetails));
 
     }
+
     @PostMapping("/calendar/spraying/{yearmonthday}/{myPlantNo}")
-    public ResponseEntity<SprayingDayResponstDto> checkSpraying(@PathVariable String yearmonthday, @PathVariable Long myPlantNo, @AuthenticationPrincipal UserDetailsImpl userDetails){
+    public ResponseEntity<SprayingDayResponstDto> checkSpraying(@PathVariable String yearmonthday, @PathVariable Long myPlantNo, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         String year = yearmonthday.substring(0, 4);
         String month = yearmonthday.substring(4, 6);
         String day = yearmonthday.substring(6);
         String date = year + "-" + month + "-" + day;
         LocalDate thatDay = LocalDate.parse(date);
-        return ResponseEntity.status(HttpStatus.OK).body(calendarService.checkSprayingInCalendar(myPlantNo, thatDay,userDetails));
+        return ResponseEntity.status(HttpStatus.OK).body(calendarService.checkSprayingInCalendar(myPlantNo, thatDay, userDetails));
     }
 
     @DeleteMapping("/calendar/spraying/{yearmonthday}/{myPlantNo}")
-    public ResponseEntity<?> deleteBloomingDay(@PathVariable Long myPlantNo,@PathVariable String yearmonthday, @AuthenticationPrincipal UserDetailsImpl userDetails){
+    public ResponseEntity<?> deleteBloomingDay(@PathVariable Long myPlantNo, @PathVariable String yearmonthday, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         String year = yearmonthday.substring(0, 4);
-        String month = yearmonthday.substring(4,6);
+        String month = yearmonthday.substring(4, 6);
         String day = yearmonthday.substring(6);
-        String date = year + "-"+month+"-"+day;
+        String date = year + "-" + month + "-" + day;
         LocalDate thatDay = LocalDate.parse(date);
 
-        return ResponseEntity.status(HttpStatus.OK).body(calendarService.delSprayingDay(myPlantNo,thatDay,userDetails));
+        return ResponseEntity.status(HttpStatus.OK).body(calendarService.delSprayingDay(myPlantNo, thatDay, userDetails));
 
     }
-
+//캘린더에서 투두취소하기
+    @PatchMapping("/calendar/cancel/{yearmonthday}/{myPlantNo}/{workType}")
+    public ResponseEntity<String> cancelTodoInCalendar(@PathVariable String yearmonthday,
+                                                       @PathVariable Long myPlantNo,
+                                                       @PathVariable String workType,
+                                                       @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        User user = userDetails.getUser();
+        String year = yearmonthday.substring(0, 4);
+        String month = yearmonthday.substring(4, 6);
+        String day = yearmonthday.substring(6);
+        String date = year + "-" + month + "-" + day;
+        LocalDate thatDay = LocalDate.parse(date);
+        try {
+            Todo todo = todoRepository.findByUserAndTodoTimeAndWorkTypeAndMyPlant_MyPlantNo(user, thatDay, workType, myPlantNo);
+            todo.setStatus(false);
+            todoRepository.save(todo);
+        } catch (Exception e) {
+            new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body("투두취소완료");
+    }
 }
+
