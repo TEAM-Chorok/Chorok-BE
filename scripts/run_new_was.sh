18e86c345f76e466bf5e98e3c2179d72506d4493
lincoln-burrows <minseong.kim7000@gmail.com>
2022-05-26 03:30:31 +0900

    [Fix]CI/CD

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -18,6 +18,7 @@ echo "> CUSTOMPASSWORD = ${CUSTOMPASSWORD}"
 
 TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
 
+
 if [ ! -z ${TARGET_PID} ]; then
   echo "> Kill WAS running at ${TARGET_PORT}."
   sudo kill ${TARGET_PID}
