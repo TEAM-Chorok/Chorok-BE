62aa0123e82dd54de2e6c993557b1bf6a77aab1c
lincoln-burrows <minseong.kim7000@gmail.com>
2022-05-25 22:05:21 +0900

    [Fix]프로필 수정기능

--- src/main/java/com/finalproject/chorok/mypage/controller/MypageController.java
+++ src/main/java/com/finalproject/chorok/mypage/controller/MypageController.java
@@ -135,14 +135,15 @@ public class MypageController {
     }
 
     //프로필 수정하기
-    @PatchMapping("/user/update/profile")
-    public ResponseEntity<HashMap<String, String>> updateProfile(@RequestBody ProfileUpdateDto profileUpdateDto,
-//            @RequestParam(value = "nickname", required = false) String nickname,
-//            @RequestParam(value = "profileImgUrl", required = false) MultipartFile multipartFile,
-//            @RequestParam(value = "profileMsg", required = false) String profileMsg,
+    @PostMapping("/user/update/profile")
+    public String updateProfile(
+            @RequestParam(value = "nickname", required = false) String nickname,
+            @RequestParam(value = "profileImgUrl", required = false) MultipartFile multipartFile,
+            @RequestParam(value = "profileMsg", required = false) String profileMsg,
+            @RequestParam(value = "originalUrl", required = false) String originalUrl,
             @AuthenticationPrincipal UserDetailsImpl userDetails) throws IOException {
         System.out.println("컨트롤러 들어오나");
-        return ResponseEntity.status(HttpStatus.OK).body(mypageService.updateProfile(profileUpdateDto,userDetails));
+        return mypageService.updateProfile(nickname,multipartFile, profileMsg, originalUrl, userDetails);
     }
 
     //회원 비활성화
