fb4627acfa674f4ba0d61d45c85b17b886aeba52
Eunah323 <unapretty@gmail.com>
2022-06-01 20:19:05 +0900

    [fit] Todo자동생성 shedrock 생성

--- src/main/java/com/finalproject/chorok/common/utils/ScheduledTodo2.java
+++ src/main/java/com/finalproject/chorok/common/utils/ScheduledTodo2.java
@@ -6,25 +6,32 @@ import com.finalproject.chorok.myPlant.repository.MyPlantRepository;
 import com.finalproject.chorok.todo.model.Todo;
 import com.finalproject.chorok.todo.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Slf4j
 @Component
 @EnableScheduling // 아래의 클래스를 스케줄링 목적으로 사용하도록 하겠다는 명시
 @Configuration
 @RequiredArgsConstructor
+@EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
 public class ScheduledTodo2 {
 
     private final TodoRepository todoRepository;
     private final MyPlantRepository myPlantRepository;
 
     //매일 00시 00분에 아래의 행위 반복
-    @Scheduled(cron = "0 19 23 * * *")
+    @Scheduled(cron = "0 0 0 * * *")
+    @SchedulerLock(name="SchedulerLock",lockAtMostFor = "PT30S", lockAtLeastFor = "PT30S")
     public void autoTodo() {
         String water = "물주기";
         String changing = "분갈이";
@@ -46,8 +53,11 @@ public class ScheduledTodo2 {
                 LocalDate todoTime = LocalDate.now();
                 boolean status = false;
                 Todo todo1 = new Todo(workType, lastWorkTime, todoTime, status, user, myPlant);
-                if (!todoRepository.existsByTodoTimeAndWorkTypeAndMyPlant(LocalDate.now(), water, myPlant))
+                if (!todoRepository.existsByTodoTimeAndWorkTypeAndMyPlant(LocalDate.now(), water, myPlant)){
                     todoRepository.save(todo1);
+                    System.out.println("워터링저장완");
+                }
+
 
             }
         }
@@ -60,9 +70,10 @@ public class ScheduledTodo2 {
                 LocalDate todoTime = LocalDate.now();
                 boolean status = false;
                 Todo todo1 = new Todo(workType, lastWorkTime, todoTime, status, user, myPlant);
-                if (!todoRepository.existsByTodoTimeAndWorkTypeAndMyPlant(LocalDate.now(), changing, myPlant))
-
+                if (!todoRepository.existsByTodoTimeAndWorkTypeAndMyPlant(LocalDate.now(), changing, myPlant)){
                     todoRepository.save(todo1);
+                    System.out.println("분갈이저장완");
+                }
 
             }
         }
@@ -75,9 +86,13 @@ public class ScheduledTodo2 {
                 LocalDate todoTime = LocalDate.now();
                 boolean status = false;
                 Todo todo1 = new Todo(workType, lastWorkTime, todoTime, status, user, myPlant);
-                if (!todoRepository.existsByTodoTimeAndWorkTypeAndMyPlant(LocalDate.now(), supplement, myPlant))
-
+                if (!todoRepository.existsByTodoTimeAndWorkTypeAndMyPlant(LocalDate.now(), supplement, myPlant)){
                     todoRepository.save(todo1);
+                    System.out.println("영양제저장완");
+                }
+
+
+
 
             }
         }
@@ -90,9 +105,12 @@ public class ScheduledTodo2 {
                 LocalDate todoTime = LocalDate.now();
                 boolean status = false;
                 Todo todo1 = new Todo(workType, lastWorkTime, todoTime, status, user, myPlant);
-                if (!todoRepository.existsByTodoTimeAndWorkTypeAndMyPlant(LocalDate.now(), cleaning, myPlant))
-
+                if (!todoRepository.existsByTodoTimeAndWorkTypeAndMyPlant(LocalDate.now(), cleaning, myPlant)){
                     todoRepository.save(todo1);
+                    System.out.println("잎닦기저장완");
+                }
+
+
 
             }
         }
@@ -105,9 +123,12 @@ public class ScheduledTodo2 {
                 LocalDate todoTime = LocalDate.now();
                 boolean status = false;
                 Todo todo1 = new Todo(workType, lastWorkTime, todoTime, status, user, myPlant);
-                if (!todoRepository.existsByTodoTimeAndWorkTypeAndMyPlant(LocalDate.now(), windy, myPlant))
+                if (!todoRepository.existsByTodoTimeAndWorkTypeAndMyPlant(LocalDate.now(), windy, myPlant)){
 
                     todoRepository.save(todo1);
+                    System.out.println("환기저장완");
+                }
+
 
             }
         }
